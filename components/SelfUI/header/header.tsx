134c740e15b9b3357bcdd295ff81fa9ebda3b191
Hleb4ikk <hlebor2006@mail.ru>
2024-11-19 15:54:24 +0300

    try to deploy 5 search in header was wrapped in suspense

--- components/SelfUI/header/header.tsx
+++ components/SelfUI/header/header.tsx
@@ -5,22 +5,21 @@ import UserMenu from "../user-menu/user-menu";
 import GeolocationComponent from "../geolocation/geolocation";
 import { Bell } from "lucide-react";
 import Search from "../search";
+import { Suspense } from "react";
 
-export default function Header(
-  {
-    searchParams,
-    width,
-  }: {
-    searchParams?: {
-      query?: string;
-      page?: string;
-    };
-    width : number;
-  },
-) {
+export default function Header({
+  searchParams,
+  width,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+  width: number;
+}) {
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
-  console.log(query+currentPage)
+  console.log(query + currentPage);
   return (
     <>
       <header className="w-full sticky items-center pl-3 pr-3 md:pl-10 md:pr-10 pt-3">
@@ -33,7 +32,10 @@ export default function Header(
             </div>
             {!(width < 768) && (
               <div className="">
-                <Search placeholder="Search posts..." className="text-white bg-[#1e1e1e] border-[#363636] border-2 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-2 placeholder:text-slate-600" />
+                <Search
+                  placeholder="Search posts..."
+                  className="text-white bg-[#1e1e1e] border-[#363636] border-2 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-2 placeholder:text-slate-600"
+                />
               </div>
             )}
           </div>
@@ -57,7 +59,12 @@ export default function Header(
         </div>
         {width < 768 && (
           <div className="pt-1 pb-1">
-            <Search placeholder="Search posts..." className="text-white bg-[#1e1e1e] border-[#363636] border-2 focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-slate-600" />
+            <Suspense>
+              <Search
+                placeholder="Search posts..."
+                className="text-white bg-[#1e1e1e] border-[#363636] border-2 focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-slate-600"
+              />
+            </Suspense>
           </div>
         )}
       </header>
