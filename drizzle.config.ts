0e862673b504126708bf8be004cb9097c98e443c
Hleb4ikk <hlebor2006@mail.ru>
2024-11-20 18:15:37 +0300

    db was created

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,13 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
 export default defineConfig({
-  out: './drizzle',
-  schema: './server/db/schema.ts',
-  dialect: 'postgresql',
+  out: "./drizzle",
+  schema: [
+    "./server/db/users.ts",
+    "./server/db/advertisements.ts",
+    "./server/db/ad-images.ts",
+  ],
+  dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
---
+++ server/db/advertisements.ts
@@ -0,0 +1,12 @@
+import { UsersTable } from './users';
+import { pgTable, timestamp, varchar, uuid } from 'drizzle-orm/pg-core';
+
+export const AdvertisementsTable = pgTable("advertisements", {
+
+    id: uuid("advertisement_id").primaryKey().defaultRandom(),
+    userId: uuid("user_id").notNull().references(()=> UsersTable.id),
+    title: varchar("title", {length: 255}).notNull(),
+    description: varchar("description", {length: 1024}).notNull(),
+    createdAt: timestamp("created_at").defaultNow(),
+
+})
