c871fd86d85e32d28645685d1bfd57ab77b0b4cb
Vlad Pavlovski <vladislav.pavlovski@gmail.com>
2025-02-13 22:14:09 +0100

    not-found fixes

--- app/(overview)/[profileIndex]/not-found.tsx
+++ app/(overview)/[profileIndex]/not-found.tsx
@@ -5,13 +5,15 @@ import { useParams } from 'next/navigation';
 const NotFound = () => {
   const params = useParams();
   return (
-    <main>
-      <div className="flex flex-col items-center space-y-4">
+    <main className="flex h-screen items-center justify-center">
+      <div className="flex flex-col items-center space-y-4 text-center">
         <h1 className="text-8xl font-bold">404</h1>
         <p className="text-3xl">
           Profile <span className="font-semibold">{params?.profileIndex}</span> not found.
         </p>
-        <Link href="/catalog">Go to catalog</Link>
+        <Link href="/catalog" className="text-blue-500 hover:underline">
+          Go to catalog
+        </Link>
       </div>
     </main>
   );
